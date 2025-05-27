710e3f106c1c35171ff738fdc0e21e61c15f822b
Matteosverona <162130834+Matteosverona@users.noreply.github.com>
2025-05-27 19:58:35 +0000

    pausa pra maria ritta

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+a = int(input("A: "))
+b = int(input("B: "))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
---
+++ comparador_idade.py
@@ -0,0 +1,2 @@
+idade_a = int(input("Qual a idade de A? "))
+idade_b = int(input("Qual a idade de B? "))
